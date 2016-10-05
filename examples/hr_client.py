0bd76bb11566ef87cbc4fcfa10387159f33c0312
Matt Wallace <mmw@lunchtimetrains.co.uk>
2016-10-05 10:54:16 +0100

    Remove advanced features not supported by basic HRM's from the example

--- examples/hr_client.py
+++ examples/hr_client.py
@@ -9,9 +9,6 @@ try:
     dev = ble.discover_device(lambda d: d['Name'] == device_name)
     dev.connect()
 
-    print "Location",dev.heart_rate_service.body_sensor_location.value
-    print "battery level",dev.battery_service.battery_level.value
-
     dev.heart_rate_service.heart_rate_measurement.notifying=True
     for i in range(10):
         print dev.heart_rate_service.heart_rate_measurement.value
